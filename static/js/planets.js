53416d6c2f2aa411a4baafeb964c7f7e94a5fdf0
ReginaCsaszar <regina.csaszar@gmail.com>
2017-07-01 19:53:56 +0200

    residents model done

--- static/js/planets.js
+++ static/js/planets.js
@@ -1,8 +1,20 @@
 $('#residentModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget)
-  var residents = button.data('info') 
-  var planet = button.data('planet')
-  var modal = $(this)
-  modal.find('.modal-title').text('Residents of ' + planet)
-  modal.find('.modal-body').text(residents)
+    var button = $(event.relatedTarget)
+    var residents = button.data('info') 
+    var planet = button.data('planet')
+    $(".temp").remove()
+    apis = residents.replace("]", "").replace("[", "").replace(/'/g, "").split(",")
+    for (let i = 0; i < apis.length; i++) {
+        $.getJSON(apis[i], function(response){
+            if (response["height"] != "unknown") {
+                 response["height"] = (parseInt(response["height"])/100).toString() + " meter"; 
+            }
+            if (response["mass"] != "unknown") {
+                 response["mass"] = response["mass"] + " kg";
+            }
+            $("table").append("<tr class='temp'><td>" + response["name"] + "</td><td>" + response["height"] + "</td><td>" + response["mass"] + "</td><td>" + response["skin_color"] + "</td><td>" + response["hair_color"] + "</td><td>" + response["eye_color"] + "</td><td>" + response["birth_year"] + "</td><td>" + response["gender"] + "</td></tr>");
+        });
+    }  
+    var modal = $(this)
+    modal.find('.modal-title').text('Residents of ' + planet)
 })
