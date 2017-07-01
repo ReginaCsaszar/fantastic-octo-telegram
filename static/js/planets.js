10eb9df1d7916ebcb54bb94bdc4e429726917c07
ReginaCsaszar <regina.csaszar@gmail.com>
2017-07-01 22:02:26 +0200

    table append bug fixed

--- static/js/planets.js
+++ static/js/planets.js
@@ -2,6 +2,8 @@ $('#residentModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget)
     var residents = button.data('info') 
     var planet = button.data('planet')
+    var modal = $(this)
+    modal.find('.modal-title').text('Residents of ' + planet)
     $(".temp").remove()
     apis = residents.replace("]", "").replace("[", "").replace(/'/g, "").split(",")
     for (let i = 0; i < apis.length; i++) {
@@ -12,9 +14,9 @@ $('#residentModal').on('show.bs.modal', function (event) {
             if (response["mass"] != "unknown") {
                  response["mass"] = response["mass"] + " kg";
             }
-            $("table").append("<tr class='temp'><td>" + response["name"] + "</td><td>" + response["height"] + "</td><td>" + response["mass"] + "</td><td>" + response["skin_color"] + "</td><td>" + response["hair_color"] + "</td><td>" + response["eye_color"] + "</td><td>" + response["birth_year"] + "</td><td>" + response["gender"] + "</td></tr>");
+            modal.find('.modal-body table').append("<tr class='temp'><td>" + response["name"] + "</td><td>" + response["height"] + "</td><td>" + response["mass"] + "</td><td>" + response["skin_color"] + "</td><td>" + response["hair_color"] + "</td><td>" + response["eye_color"] + "</td><td>" + response["birth_year"] + "</td><td>" + response["gender"] + "</td></tr>");
         });
     }  
-    var modal = $(this)
-    modal.find('.modal-title').text('Residents of ' + planet)
 })
+var modal = $(this)
+    modal.find('.modal-title').text('Residents of ' + planet)
